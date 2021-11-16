aa4a0e45c7517d21174a0498f517660a302a6f6f
brianorora <brianorora@gmail.com>
2021-11-16 19:32:09 +0300

    Add `Link` to `<Details />` page

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import Pokemon from './components/Pokemon';
 import styles from './Homepage.module.css';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   const [pokemons, setPokemons] = useState([]);
@@ -19,7 +20,9 @@ const Homepage = () => {
   return (
     <div className={styles.container}>
       {pokemons.map((data) => (
-        <Pokemon data={data}/>
+        <Link to={`details/${data.name}`}>
+          <Pokemon data={data}/>
+        </Link>
       ))}
     </div>
   );
