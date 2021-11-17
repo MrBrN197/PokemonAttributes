4a2a4435daeac8f9c3316cd3c13f2c14725a7555
brianorora <brianorora@gmail.com>
2021-11-17 17:27:36 +0300

    Add `Navbar` h1 link & back button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,16 @@
 /* eslint-disable */
 import styles from './Navbar.module.scss';
+import { IoIosArrowBack } from 'react-icons/io';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <header className={styles.header}>
+    <div className={styles.back}>
+      <IoIosArrowBack size="100%" />
+    </div>
+    <Link to="/">
+      <h1>Pokemon metrics</h1>
+    </Link>
   </header>
 );
 
