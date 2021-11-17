4a1dcf83aa897b0c2e30eed252eefd24e40d9989
brianorora <brianorora@gmail.com>
2021-11-17 19:37:10 +0300

    Add back icon navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,20 @@
-/* eslint-disable */
-import styles from './Navbar.module.scss';
-import { IoIosArrowBack } from 'react-icons/io';
+import { IoIosArrowBack, IoIosSettings } from 'react-icons/io';
+
 import { Link } from 'react-router-dom';
+
+import styles from './Navbar.module.scss';
 
 const Navbar = () => (
   <header className={styles.header}>
-    <div className={styles.back}>
+    <Link to="/" className={styles.back}>
       <IoIosArrowBack size="100%" />
-    </div>
+    </Link>
     <Link to="/">
       <h1>Pokemon metrics</h1>
     </Link>
+    <div className={styles.settings}>
+      <IoIosSettings size="100%" />
+    </div>
   </header>
 );
 
