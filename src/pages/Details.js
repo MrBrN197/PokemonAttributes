471452023903a639b9b72f3ff19ca2b469958b65
brianorora <brianorora@gmail.com>
2021-11-17 12:39:10 +0300

    Change error & loading message `font-color`

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -12,7 +12,6 @@ const Details = () => {
         const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
         const respData = await resp.json();
         setDetails({ loading: false, data: respData });
-        console.log(respData);
       } catch {
         setDetails({ loading: false, error: true });
       }
@@ -20,11 +19,12 @@ const Details = () => {
     fetchPokemonDetails();
   }, [name]);
 
-  if (details.error) return <h3>Pokemon Not Found</h3>;
-  if (details.loading) return <h3>Loading...</h3>;
+  if (details.error) return <h3 style={{ color: 'black' }}>Pokemon Not Found</h3>;
+  if (details.loading) return <h3 style={{ color: 'black' }}>Loading...</h3>;
 
   return (
     <div className={styles.container}>
+      <h2>{name[0].toUpperCase() + name.slice(1)}</h2>
       <img src={details.data.sprites.front_default} alt={name} />
       <ul className={styles.types}>
         {details.data.types.map((type) => (
@@ -46,14 +46,15 @@ const Details = () => {
         </p>
       </div>
       <ul className={styles.stats}>
-        {details.data.stats.map((stat) => (
-          <li key={stat.stat.name}>
-            {stat.stat.name}
-            :
-            {' '}
-            {stat.base_stat}
-          </li>
-        ))}
+        <span className={styles.heading}>Stats</span>
+        <div>
+          {details.data.stats.map((stat) => (
+            <li key={stat.stat.name}>
+              <span>{stat.stat.name[0].toUpperCase() + stat.stat.name.slice(1)}</span>
+              <span>{stat.base_stat}</span>
+            </li>
+          ))}
+        </div>
       </ul>
     </div>
   );
