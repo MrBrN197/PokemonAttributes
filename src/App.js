d3f9fdac51a26f2413b44a6ef67e36ac28ce02ab
brianorora <brianorora@gmail.com>
2021-11-17 16:45:53 +0300

    Add `Navbar` component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Details from './pages/Details';
+import Navbar from './components/Navbar';
 
 const App = () => (
-  <Switch>
-    <Route path="/" exact>
-      <Homepage />
-    </Route>
-    <Route path="/pokemon/:name">
-      <Details />
-    </Route>
-  </Switch>
+  <>
+    <Navbar />
+    <Switch>
+      <Route path="/" exact>
+        <Homepage />
+      </Route>
+      <Route path="/pokemon/:name">
+        <Details />
+      </Route>
+    </Switch>
+  </>
 );
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import styles from './Navbar.module.scss';
+
+const Navbar = () => (
+  <header className={styles.header}>
+  </header>
+);
+
+export default Navbar;
