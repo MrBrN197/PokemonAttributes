4509cb53502d5bd98ff9f2c70db16ddddb7653de
brianorora <brianorora@gmail.com>
2021-11-18 14:16:59 +0300

    Hide back button in Homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,27 @@
 import { IoIosArrowBack, IoIosSettings } from 'react-icons/io';
 
-import { Link } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 import styles from './Navbar.module.scss';
 
-const Navbar = () => (
-  <header className={styles.header}>
-    <Link to="/" className={styles.back}>
-      <IoIosArrowBack size="100%" />
-    </Link>
-    <Link to="/">
-      <h1>Pokemon metrics</h1>
-    </Link>
-    <div className={styles.settings}>
-      <IoIosSettings size="100%" />
-    </div>
-  </header>
-);
+const Navbar = () => {
+  const match = useRouteMatch('/pokemon');
+
+  return (
+    <header className={styles.header}>
+      {match && (
+      <Link to="/" className={styles.back}>
+        <IoIosArrowBack size="100%" />
+      </Link>
+      )}
+      <Link to="/">
+        <h1>Pokemon metrics</h1>
+      </Link>
+      <div className={styles.settings}>
+        <IoIosSettings size="100%" />
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;
