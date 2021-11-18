18abad37a5d8e5c70c35339dfbb699671e20f2da
brianorora <brianorora@gmail.com>
2021-11-18 14:53:58 +0300

    Add pokemon by name search functionality

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,26 +1,35 @@
-/* eslint-disable */
+import { useEffect, useState } from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Pokemon from '../components/Pokemon';
 import styles from './Homepage.module.scss';
-import { Link, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 const Homepage = () => {
   let pokemons = useSelector((state) => state.pokemons);
 
-  const location  = useLocation();
-  console.log(location)
+  const location = useLocation();
   const query = new URLSearchParams(location.search);
   const search = query.get('search') || '';
-  pokemons = pokemons.filter( p => p.name.includes(search))
+  pokemons = pokemons.filter((p) => p.name.includes(search));
+  const history = useHistory();
+  const [searchValue, setSearchValue] = useState(search);
+
+  const handleSearch = (e) => {
+    history.push(e.target.value ? `?search=${e.target.value}` : '');
+    setSearchValue(e.target.value);
+  };
 
   return (
-    <div className={styles.container}>
-      {pokemons.map((data) => (
-        <Link key={data.name} to={`pokemon/${data.name}`}>
-          <Pokemon data={data}/>
-        </Link>
-      ))}
-    </div>
+    <>
+      <input onChange={handleSearch} type="text" value={searchValue} />
+      <div className={styles.container}>
+        {pokemons.map((data) => (
+          <Link key={data.name} to={`pokemon/${data.name}`}>
+            <Pokemon data={data} />
+          </Link>
+        ))}
+      </div>
+    </>
   );
 };
 
